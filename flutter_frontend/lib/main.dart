e57108995a84f5fdb506110e3a41a7ce3df2babd
Maxime Haurel <54206618+MHaurel@users.noreply.github.com>
2024-02-25 11:35:21 +0100

    fix: minor changes to install

--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_frontend/pages/home_page.dart';
-import 'package:flutter_frontend/pages/test_page.dart';
 
 void main() {
   runApp(const MainApp());

5a9db41a6d0db45fdf1ec4af2bb743da798ae3b6
enoraa <81618564+enoraa@users.noreply.github.com>
2024-03-07 20:36:01 +0100

    Navigation section page pays
    Ajout du laucnher icon
    Base Navigation entre sections de la page pays

---
+++ flutter_frontend/lib/widgets/country_sections_navigation.dart
@@ -0,0 +1,81 @@
+//navigation menu on the right of the country page to easily navigate between the sections of the page
+import 'package:flutter/material.dart';
+import 'package:flutter_frontend/widgets/menu_item.dart';
+import 'package:sticky_headers/sticky_headers.dart';
+
+class CountrySectionsNavigation extends StatelessWidget {
+  final ScrollController _scrollController = ScrollController();
+  final List<String> _sections = [
+    'Sécurité',
+    'Climat sociopolitique',
+    'Droits des femmes et enfants',
+    'Droit LGBT+',
+    'Allergènes'
+    'Us et coutumes',
+    'Climat',
+    'Conditions sanitaires',
+    'Espace collaboratif',
+  ];  //a modifier avec les Risk des country.dart 
+
+
+  void _scrollToSection() {
+    // You can change the yOffset value according to your layout
+    double yOffset = 300.0;
+    _scrollController.animateTo(
+      yOffset,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      child : StickyHeader(
+        header: Column(
+          children: <Widget>[
+            for (int i = 0; i < _sections.length; i++)
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  MenuItem(
+                    onPressed: () => _scrollToSection(),
+                    text: _sections[i],
+                  ),
+                ],
+            ),
+          ],
+        ),
+        content: SizedBox(
+          height: MediaQuery.of(context).size.height * 4.4,
+          width: 300,
+        ),
+      ),
+    );
+          //       ),
+          //       ElevatedButton(
+          //       onPressed: _scrollToSection,
+          //       child: Text('Scroll To Section'),
+          //     ),
+          //   ),
+          //   Center(
+          //     child: Container(
+          //       height: 300,
+          //       width: 300,
+          //       color: Colors.blue,
+          //       child: Center(
+          //         child: Text(
+          //           'Section to scroll to',
+          //           style: TextStyle(
+          //             color: Colors.white,
+          //           ),
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          //   SizedBox(height: 100), // Placeholder space to demonstrate scrolling
+          // ],
+    //   ),
+    // );
+  }
+}
---
+++ flutter_frontend/lib/widgets/menu_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MenuItem extends StatelessWidget {
+  const MenuItem({super.key, required this.onPressed, required this.text});
+
+  final Function() onPressed;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      style: ButtonStyle(
+        side: MaterialStateProperty.all(BorderSide(
+          color: Colors.transparent,
+          width: 1.0,
+          strokeAlign: BorderSide.strokeAlignInside,
+        )),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: const Color(0xFF07020D),
+              fontFamily: Theme.of(context).textTheme.bodyMedium!.fontFamily,
+              fontSize: Theme.of(context).textTheme.bodyMedium!.fontSize,
+              fontWeight: Theme.of(context).textTheme.bodyMedium!.fontWeight,
+            ),
+            textAlign: TextAlign.left, // align text to the left of the button
+          ),
+        ),
+      ),
+    );
+  }
+}
